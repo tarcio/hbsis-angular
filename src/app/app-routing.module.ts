7b8f49c825a6b9975d97dee8fe47c5c8a6812745
Tarcísio Oliveira <tarc.lima@gmail.com>
2019-01-27 22:57:24 -0200

    update app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CityPageComponent } from './components/city-page/city-page.component';
+import { CityForecastPageComponent } from './components/city-forecast-page/city-forecast-page.component';
+
+const routes: Routes = [{
+	path: 'city',
+	component: CityPageComponent
+}, {
+	path: 'city/:name',
+	component: CityForecastPageComponent
+}, {
+	path: '',
+	redirectTo: '/city',
+	pathMatch: 'full'
+}, {
+	path: '**',
+	redirectTo: '/city',
+	pathMatch: 'full'
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CityPageComponent } from './components/city-page/city-page.component';
+import { CityForecastPageComponent } from './components/city-forecast-page/city-forecast-page.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		CityPageComponent,
+		CityForecastPageComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		HttpClientModule,
+		MatButtonModule,
+		MatCardModule,
+		MatFormFieldModule,
+		MatIconModule,
+		MatInputModule,
+		MatPaginatorModule,
+		MatSnackBarModule,
+		MatTableModule,
+		MatToolbarModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
